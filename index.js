75db273af49811338ea332bf22321ba8e6c56651
Hannah Lee <hannahyvonne16@gmail.com>
2022-01-31 19:02:10 -0800

    1st commit

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+
+window.prompt(weeklyExpenseQuestions[0])
+let answer = window.prompt(weeklyExpesnseQuestions[0])
+console.log(answer)
